296445d99afe3b9c2d43396649e6ae0d6c7006fd
Abdul Razzaq <arazaq917@gmail.com>
2023-11-07 00:42:16 +0500

    error resolved

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,10 @@
-import Header from "./header";
 import React from "react";
 import PaperCalculator from "../components";
 
 export default function Home() {
   return (
     <div>
-        <PaperCalculator/>
+        <PaperCalculator initialValue={Math.floor(Math.random() * (99999999 - 10000000 + 1) + 10000000)}/>
     </div>
   )
 }
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,11 +1,10 @@
 'use client'
 import React, {useEffect, useState} from "react";
-import Link from "next/link";
 import Header from "../app/header";
 import PaperBenefits from "./resourcePage";
 import Footer from "./footer";
 
-const PaperCalculator = () => {
+const PaperCalculator = ({initialValue}) => {
     const [numberOfPages, setNumberOfPages] = useState(1000);
     const [results, setResults] = useState({
         CO2_saved: 0,
@@ -16,17 +15,14 @@ const PaperCalculator = () => {
         salary_saved: 0,
         cost_decrease_percentage: 0
     });
-    const initialTrees = Math.floor(Math.random() * (99999999 - 10000000 + 1) + 10000000);
 
     const BASE_PAPER_CERTIFICATE_COST = 30;
 
-    const [treesCut, setTreesCut] = useState(initialTrees);
+    const [treesCut, setTreesCut] = useState(initialValue);
 
     useEffect(() => {
         calculateValues(1000)
         const interval = setInterval(() => {
-            const randomFloat = Math.random();
-            console.log(randomFloat)
             setTreesCut(prevTrees => prevTrees + 0.01);
         }, 10);
         return () => clearInterval(interval);
@@ -79,130 +75,13 @@ const PaperCalculator = () => {
                                     </span>
                                 </div>
                                 <span className="text-white font-bold my-8 px-[30px] rounded-[20px] text-[125px] w-full bg-[#FFFFFF33]">
-                                    {treesCut.toLocaleString()}
+                                    {treesCut?.toLocaleString()}
                                 </span>
                                 <span className="lg:text-[42px] text-center flex flex-col">
                                     <span>Hectares of Trees & Forest <strong>Cut Down</strong> or <strong>Burned</strong></span>
                                     <span className="font-bold">This Year!</span>
                                 </span>
                             </div>
-
-                            {/*<div className="flex flex-col gap-3 items-center sm:w-full lg:w-2/4">*/}
-                            {/*    <h1 className="text-white sm:text-2xl lg:text-4xl font-bold mb-4 text-center w-11/12">*/}
-                            {/*        Please enter the estimated number of paper certificates issued annually*/}
-                            {/*    </h1>*/}
-
-                            {/*    <div className="mb-4 w-72">*/}
-                            {/*        <input*/}
-                            {/*            onWheel={numberInputOnWheelPreventChange}*/}
-                            {/*            value={numberOfPages}*/}
-                            {/*            onChange={calculatePaper}*/}
-                            {/*            type="number"*/}
-                            {/*            placeholder="Enter number"*/}
-                            {/*            className="w-full p-2 border rounded-md bg-gray-200 m-3 h-16 text-2xl text-black"*/}
-                            {/*        />*/}
-                            {/*    </div>*/}
-
-                            {/*    <p className="text-white text-sm text-gray-500 w-11/12">*/}
-                            {/*        *In order to obtain valuable cost and time-saving insights, it is essential to input*/}
-                            {/*        a minimum of 200 certificates.*/}
-                            {/*    </p>*/}
-                            {/*</div>*/}
-                            {/*<div className="flex flex-col gap-10 items-center sm:w-full lg:w-2/4">*/}
-                            {/*    <h1 className="text-white text-3xl font-bold mb-4">*/}
-                            {/*        By Switching to Blockchain Certificate with MyEduID*/}
-                            {/*    </h1>*/}
-                            {/*    <div className="flex flex-col sm:flex-row mt-10">*/}
-                            {/*        <div className="mr-10">*/}
-                            {/*            <h2 className="text-white text-2xl font-medium mb-2">*/}
-                            {/*                You can save <span*/}
-                            {/*                className="font-bold text-3xl">RM {results.money_saved}</span> per year*/}
-                            {/*            </h2>*/}
-
-                            {/*            <p className="text-gray-600">*/}
-                            {/*                By cutting down material, printing, labour cost, storage, packaging &*/}
-                            {/*                delivery*/}
-                            {/*            </p>*/}
-                            {/*        </div>*/}
-
-                            {/*        <div className="flex-shrink-0 flex justify-center">*/}
-                            {/*            <div*/}
-                            {/*                className="h-48 w-48 border-2 rounded-full border-gray-300 flex items-center justify-center">*/}
-                            {/*                <span className="text-gray-500">Image</span>*/}
-                            {/*            </div>*/}
-                            {/*        </div>*/}
-                            {/*    </div>*/}
-                            {/*    <div className="text-white flex flex-col sm:flex-row mt-10">*/}
-                            {/*        <div className="mr-10">*/}
-                            {/*            <h2 className="text-white text-2xl font-medium mb-2">*/}
-                            {/*                <span className="font-bold text-3xl">{results.CO2_saved}g</span> Carbon*/}
-                            {/*                Dioxide Reduction*/}
-                            {/*            </h2>*/}
-
-                            {/*            <p className="text-gray-600">*/}
-                            {/*                Control greenhouse gas emissions, scoring win for a sustainable future*/}
-                            {/*            </p>*/}
-                            {/*        </div>*/}
-
-                            {/*        <div className="flex-shrink-0 flex justify-center">*/}
-                            {/*            <div*/}
-                            {/*                className="h-48 w-48 border-2 rounded-full border-gray-300 flex items-center justify-center">*/}
-                            {/*                <span className="text-gray-500">Image</span>*/}
-                            {/*            </div>*/}
-                            {/*        </div>*/}
-                            {/*    </div>*/}
-                            {/*    <div className="text-white flex flex-col sm:flex-row">*/}
-                            {/*        <div className="mr-10">*/}
-                            {/*            <h2 className="text-2xl font-medium mb-2">*/}
-                            {/*                Saved <span*/}
-                            {/*                className="font-bold text-3xl">{results.trees_saved} Trees</span>*/}
-                            {/*            </h2>*/}
-
-                            {/*            <p className="text-gray-600">*/}
-                            {/*                Saved trees from paper production plus trees that need to absorb <span*/}
-                            {/*                className="font-medium">{results.CO2_saved / 1000}kg</span> of CO2*/}
-                            {/*            </p>*/}
-                            {/*        </div>*/}
-
-                            {/*        <div className="flex-shrink-0 flex justify-center">*/}
-                            {/*            <div*/}
-                            {/*                className="h-48 w-48 border-2 rounded-full border-gray-300 flex items-center justify-center">*/}
-                            {/*                <span className="text-gray-500">Image</span>*/}
-                            {/*            </div>*/}
-                            {/*        </div>*/}
-                            {/*    </div>*/}
-                            {/*    <div className="text-white flex flex-col sm:flex-row">*/}
-                            {/*        <div className="mr-10">*/}
-                            {/*            <h2 className="text-2xl font-medium mb-2">*/}
-                            {/*                <span*/}
-                            {/*                    className="font-bold text-3xl">{results.time_saved_hours.toFixed(0)} Hours</span> productivity*/}
-                            {/*                hours saved annually*/}
-                            {/*            </h2>*/}
-
-                            {/*            <p className="text-gray-600">*/}
-                            {/*                That is a <span*/}
-                            {/*                className="font-medium">RM {results.salary_saved.toFixed(0)}</span> salary*/}
-                            {/*                saved in a year*/}
-                            {/*            </p>*/}
-                            {/*        </div>*/}
-
-                            {/*        <div className="flex-shrink-0 flex justify-center">*/}
-                            {/*            <div*/}
-                            {/*                className="h-48 w-48 border-2 rounded-full border-gray-300 flex items-center justify-center">*/}
-                            {/*                <span className="text-gray-500">Image</span>*/}
-                            {/*            </div>*/}
-                            {/*        </div>*/}
-                            {/*    </div>*/}
-                            {/*    <div className="text-white text-center">*/}
-                            {/*        <h2 className="text-3xl font-bold mb-4">Join Our Movement</h2>*/}
-                            {/*        <Link href="https://www.myeduid.my/contact">*/}
-                            {/*            <button*/}
-                            {/*                className="bg-gray-400 text-white px-6 py-3 rounded-full hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">*/}
-                            {/*                Contact Us*/}
-                            {/*            </button>*/}
-                            {/*        </Link>*/}
-                            {/*    </div>*/}
-                            {/*</div>*/}
                         </div>
                     </div>
                 </div>
